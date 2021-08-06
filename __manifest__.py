557b3371d232a1d1558899371224364a19c6c362
ftoure@eaglesoftconsulting.com <caucus@esc78>
2021-08-06 16:15:55 +0000

    Security Configuration

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/eaglefuel_view.xml',
         #'views/views.xml',
         'views/templates.xml',
