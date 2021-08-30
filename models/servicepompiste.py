60c76f20a1902f0839dbeca5c0065f44795e065e
keita <kmahamadou858@gmail.com>
2021-08-30 14:38:16 +0000

    finissons de l'ajout de la fonction name_get

--- models/servicepompiste.py
+++ models/servicepompiste.py
@@ -13,11 +13,19 @@ class servicepompiste(models.Model):
     temps_debut = fields.Float("Heure de depart", required=True)
     temps_fin = fields.Float("Heure de fin")
     duree = fields.Float("Duree du service")
+    index_depart = fields.Integer("Index de depart")
+    index_arrive = fields.Integer("Index d'arrive")
+    litrage = fields.Float("Litrage vendu")
+    litrage_credit = fields.Float("Litrage vendu a credit")
+    montant_credit = fields.Float("Somme d'argent a credit")
+    montant_total_vendu = fields.Integer("Total vendu")
+    montant_a_verse = fields.Integer("Montant a verse")
+    montant_verse = fields.Integer("Montant verse")
+    ecart = fields.Integer("Ecart du compte")
     qm_id = fields.Many2many("hr.employee", string="QM_responsables")
     pompiste_id = fields.Many2one("hr.employee", string="Pompiste responsable")
     pompe_id = fields.Many2one("eaglefuel.pompe", string="pompe utilise")
     versement_id = fields.One2many("eaglefuel.versement", "servicepompiste_id", string="versement")
-
 
     def name_get(self):
         result = []
