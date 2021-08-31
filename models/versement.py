08d4dc42f181794f50a29c4d638ea59b11b52a37
keita <kmahamadou858@gmail.com>
2021-08-31 16:15:19 +0000

    amelioration de la classe service pompiste

--- models/versement.py
+++ models/versement.py
@@ -5,12 +5,12 @@ from odoo import models, fields, api
 class versement(models.Model):
     _name = "eaglefuel.versement"
     _description = "versement"
-    _rec_name = "ref"
+    # _rec_name = "ref"
 
     ref = fields.Char("reference")
     montant_versement = fields.Integer("Montant du versement")
-    heure_versement = fields.Float("Heure du versement", required=True)
-    nbre_versement = fields.Integer("Nombre de versement")
+    # heure_versement = fields.Float("Heure du versement", required=True)
+    # nbre_versement = fields.Integer("Nombre de versement")
     total_versement = fields.Integer("Total somme verse")
     servicepompiste_id = fields.Many2one("eaglefuel.servicepompiste", string="service pompiste")
 
